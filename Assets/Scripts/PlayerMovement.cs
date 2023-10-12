7af5d8393958ec3b59629241563bfd71258b1be3
João Ragazzo <joao_ragazzo@hotmail.com>
2023-10-11 23:02:17 -0300

    fix: fixing player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -47,19 +47,25 @@ public class PlayerMovement : MonoBehaviour
         
         #region Movement script
         
-        Vector3 foward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
+        //Vector3 foward = transform.TransformDirection(Vector3.forward);
+        //Vector3 right = transform.TransformDirection(Vector3.right);
+        
+        Vector3 forward = Vector3.forward;
+        Vector3 right = Vector3.right;
 
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0 ;
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0 ;
         //float movementDirectionY = moveDirection.y;
         
-        moveDirection = (foward * curSpeedX) + (right * curSpeedY);    
-        
-        if (!characterController.isGrounded)
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        // Verifica se o jogador está se movendo diagonalmente (ambos os eixos têm entrada)
+        if (Mathf.Abs(curSpeedX) > 0.1f && Mathf.Abs(curSpeedY) > 0.1f)
         {
-            moveDirection.y -= gravity * Time.deltaTime;
+            // Normaliza o vetor de movimento para garantir que a velocidade diagonal não seja mais rápida
+            moveDirection.Normalize();
+            moveDirection *= (isRunning ? runSpeed : walkSpeed); // Aplica a velocidade correta após a normalização
         }
         
         characterController.Move(moveDirection * Time.deltaTime);
