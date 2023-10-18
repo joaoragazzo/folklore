654b1ffb9646440691300eaf14b8ecdedccbea7a
Matheus <math.mds147@gmail.com>
2023-10-18 16:12:41 -0300

    Update Multiple Drone Moviment

--- Assets/Scripts/DroneMoviment.cs
+++ Assets/Scripts/DroneMoviment.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class DroneFollow : MonoBehaviour
 {
     public Transform playerTransform;
-    public float followDistance = 5.0f;
+    public float followDistance = 3.0f;
     public float safeDistance = 2.0f;
+    public float droneAvoidanceDistance = 2.0f; 
     public float moveSpeed = 5.0f;
     public Camera mainCamera; // A câmera principal do jogo, você precisa associar isso no Editor da Unity
-    public int height = 20;
+    public int height = 3;
+    
+    private static List<DroneFollow> allDrones; // lista estática contendo todas as instâncias de drones
+
+    private void Awake()
+    {
+        if (allDrones == null)
+        {
+            allDrones = new List<DroneFollow>();
+        }
+        allDrones.Add(this); // adicionar este drone à lista quando é criado/inicializado
+    }
     
     private void Update()
     {
@@ -40,6 +52,20 @@ public class DroneFollow : MonoBehaviour
             Vector3 newPosition = transform.position + directionToPlayer * moveSpeed * Time.deltaTime;
             newPosition.y = height;
             transform.position = newPosition;
+        }
+        
+        // Verifica a proximidade de outros drones e ajusta a posição para evitar colisão
+        foreach (var otherDrone in allDrones)
+        {
+            if (otherDrone != this)
+            {
+                float distanceToDrone = Vector3.Distance(transform.position, otherDrone.transform.position);
+                if (distanceToDrone < droneAvoidanceDistance)
+                {
+                    Vector3 avoidDirection = (transform.position - otherDrone.transform.position).normalized;
+                    transform.position += avoidDirection * moveSpeed * Time.deltaTime;
+                }
+            }
         }
 
         // Fazendo o drone olhar para a posição do mouse
