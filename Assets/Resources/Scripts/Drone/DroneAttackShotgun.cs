1260e07a800a6c57b6477963a41b2098f04ec139
Matheus <math.mds147@gmail.com>
2023-10-26 14:14:57 -0300

    Update DroneAttackShotgun.cs

--- Assets/Resources/Scripts/Drone/DroneAttackShotgun.cs
+++ Assets/Resources/Scripts/Drone/DroneAttackShotgun.cs
@@ -5,47 +5,42 @@ using UnityEngine;
 
 public class DroneAttackShotgun : MonoBehaviour
 {
-    public GameObject projectilePrefab; // O prefab do seu projétil; defina isso no inspetor da Unity.
-    public float projectileSpeed = 20f; // A velocidade do seu projétil.
-    public float fireRate = 1f; // Quantos tiros por segundo.
-    private float nextTimeToFire = 2f; // Controla o tempo até o próximo tiro.
-    [SerializeField] private LayerMask mouseColliderMask = new LayerMask();
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 20f;
+    public float fireRate = 1f;
+    private float nextTimeToFire = 2f;
+    [SerializeField] private LayerMask mouseColliderMask;
     [SerializeField] private Transform bulletVFX;
-    
+
+    public int pelletsCount = 8; // Quantos projéteis por disparo.
+    public float spreadAngle = 20f; // ângulo de dispersão dos projéteis.
+
     void Update()
     {
         Vector3 mouseWorldPosition = Vector3.zero;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Transform hitTransform = null;
         if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, mouseColliderMask))
         {
             Vector3 aimDir = (raycastHit.point - transform.position).normalized;
             if (Input.GetMouseButton(0) && Time.time >= nextTimeToFire)
             {
-                Instantiate(projectilePrefab, transform.position, Quaternion.LookRotation(aimDir, Vector3.up));
-                
+                for (int i = 0; i < pelletsCount; i++)
+                {
+                    FirePellet(aimDir, spreadAngle);
+                }
+
                 nextTimeToFire = Time.time + 1f/fireRate;
             }
         }
-        
     }
 
-    void Hitscan()
+    void FirePellet(Vector3 aimDir, float spread)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Transform hitTransform = null;
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, mouseColliderMask))
-        {
-            transform.position = raycastHit.point;
-            hitTransform = raycastHit.transform;
-        }
+        // Gera um desvio aleatório para a direção do projétil.
+        Vector3 spreadVector = Random.insideUnitCircle * Mathf.Tan(spread * Mathf.Deg2Rad);
+        Vector3 perturbedAimDir = Quaternion.LookRotation(aimDir) * new Vector3(spreadVector.x, spreadVector.y, 1f);
 
-        if (Input.GetMouseButton(0) && Time.time >= nextTimeToFire)
-        {
-            if (hitTransform)
-            {
-                Instantiate(bulletVFX, transform.position, Quaternion.identity);
-            }
-        }
+        // Instancia o projétil com a direção perturbada.
+        Instantiate(projectilePrefab, transform.position, Quaternion.LookRotation(perturbedAimDir));
     }
 }
