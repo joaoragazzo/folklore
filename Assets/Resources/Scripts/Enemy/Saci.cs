beb098e90f8b052e5e79e496ec90d14feeade877
João Ragazzo <joao_ragazzo@hotmail.com>
2023-11-03 17:15:49 -0300

    adding saci annimation

--- Assets/Resources/Scripts/Enemy/Saci.cs
+++ Assets/Resources/Scripts/Enemy/Saci.cs
@@ -8,9 +8,14 @@ public class Saci : MonoBehaviour, IDamageble
     private SaciStats Stats;
     private EnemyMovement _enemyMovement;
     private bool isAttacking;
+    private PlayerInteraction _playerInteraction;
+    private Animator _animator;
 
     void Start()
     {
+        _playerInteraction = new PlayerInteraction();
+        _playerInteraction.Initialize();
+        _animator = GetComponent<Animator>();
         Stats = new SaciStats();
         _enemyMovement = new EnemyMovement();
     }
@@ -23,6 +28,20 @@ public class Saci : MonoBehaviour, IDamageble
             Stats.baseAttackRange,
             isAttacking
         );
+
+        if (isAttacking)
+        {
+            _animator.SetTrigger("Saci Attacking");
+        }
+        else
+        {
+            _animator.Play("Saci Running");
+        }
+    }
+
+    public void onAttack()
+    {
+        
     }
 
     public void TakeDamage(int amount)
