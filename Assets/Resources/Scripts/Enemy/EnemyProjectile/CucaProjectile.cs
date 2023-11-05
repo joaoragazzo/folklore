752d80d8f750df878e5f2ae98b1d6c5e13cab68b
João Ragazzo <joao_ragazzo@hotmail.com>
2023-11-05 20:08:31 -0300

    feat: adding damage to cuca projectile

--- Assets/Resources/Scripts/Enemy/EnemyProjectile/CucaProjectile.cs
+++ Assets/Resources/Scripts/Enemy/EnemyProjectile/CucaProjectile.cs
@@ -5,18 +5,38 @@ public class CucaProjectile : MonoBehaviour
 {
     private CucaStats _stats;
     private Rigidbody _rigidbody;
-    private PlayerInteraction _playerInteraction;
     private float lifeDuration = 5f;
-    private float damage;
 
     public void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     public void Start()
     {
         
+        Destroy(gameObject, lifeDuration);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _stats = new CucaStats(new WorldInteraction());
+            PlayerInteraction playerInteraction = new PlayerInteraction();
+            playerInteraction.Initialize();
+            
+            Debug.Log(playerInteraction == null);
+            
+            playerInteraction.PlayerStats.Health -= _stats.baseAttackDamage;
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Tree") || other.CompareTag("Ground"))
+        {  
+            Destroy(gameObject);
+        }
+    }
+    
 }
 
