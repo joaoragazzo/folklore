d8310a9098fd4adc99c93e892adc5d0b5100a183
João Ragazzo <joao_ragazzo@hotmail.com>
2023-11-15 00:48:30 -0300

    feat/fix: adding function to the main menu & fixing lighting and player animation

---
+++ Assets/Resources/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Button startButton;
+    public Button configButton;
+    public Button creditsButton;
+    public Button exitButton;
+    public Button goBack;
+    public Button goBack2;
+
+    public GameObject mainMenuPrefab;
+    public GameObject configMenuPrefab;
+    public GameObject creditsMenuPrefab;
+    
+    
+    private void Start()
+    {
+        startButton.onClick.AddListener(startGame);
+        configButton.onClick.AddListener(viewConfig);
+        creditsButton.onClick.AddListener(viewCredits);
+        exitButton.onClick.AddListener(exitGame);
+        goBack.onClick.AddListener(goBackToMainMenu);
+        goBack2.onClick.AddListener(goBackToMainMenu);
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadSceneAsync("Scenes/MainGame");
+    }
+
+    public void viewConfig()
+    {
+        mainMenuPrefab.SetActive(false);
+        configMenuPrefab.SetActive(true);
+    }
+
+    public void viewCredits()
+    {
+        mainMenuPrefab.SetActive(false);
+        creditsMenuPrefab.SetActive(true);
+    }
+
+    public void goBackToMainMenu()
+    {
+        creditsMenuPrefab.SetActive(false);
+        configMenuPrefab.SetActive(false);
+        mainMenuPrefab.SetActive(true);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+    
+    
+}
