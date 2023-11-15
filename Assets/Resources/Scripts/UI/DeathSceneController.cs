8279893f6dc99980924520835ac028563baf39e7
João Ragazzo <joao_ragazzo@hotmail.com>
2023-11-15 00:58:58 -0300

    feat: adding func to death screen menu

---
+++ Assets/Resources/Scripts/UI/DeathSceneController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DeathSceneController : MonoBehaviour
+{
+    public Button goBackToMenu;
+    public Button repeatGame;
+
+    public void Start()
+    {
+        goBackToMenu.onClick.AddListener(goBackToMenuFunction);
+        repeatGame.onClick.AddListener(repeatGameFunction);
+    }
+
+    public void goBackToMenuFunction()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void repeatGameFunction()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
