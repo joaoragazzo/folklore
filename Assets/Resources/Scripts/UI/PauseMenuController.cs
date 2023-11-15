c96e6c26b967076ca8474cc429a65d0fe41ebec9
João Ragazzo <joao_ragazzo@hotmail.com>
2023-11-15 01:26:03 -0300

    feat: adding graphic quality options

---
+++ Assets/Resources/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menuPrefab;
+    public bool actualStateMenu;
+    public Button goBackToMenu;
+
+
+    public void Start()
+    {
+        goBackToMenu.onClick.AddListener(goBackToMenuFunc);
+
+    }
+    
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            actualStateMenu = !actualStateMenu;
+            menuPrefab.SetActive(actualStateMenu);
+            if (!actualStateMenu) WorldStatsController.Stats.Pause();
+            else WorldStatsController.Stats.Unpause();
+        }
+    }
+
+    public void goBackToMenuFunc()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Resources/Scripts/UI/QualityController.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class QualityController : MonoBehaviour
+{
+    public TMP_Dropdown qualityDropdown;
+    
+    void Start()
+    {
+        InitializeQualityDropdown();
+    }
+
+    void InitializeQualityDropdown()
+    {
+        qualityDropdown.ClearOptions();
+        
+        // Adicionar as opções de qualidade ao Dropdown
+        foreach (var name in QualitySettings.names)
+        {
+            qualityDropdown.options.Add(new TMP_Dropdown.OptionData(name));
+        }
+
+        // Define a seleção atual para a configuração atual de qualidade
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        qualityDropdown.RefreshShownValue();
+
+        // Adicionar um ouvinte para quando a seleção mudar
+        qualityDropdown.onValueChanged.AddListener(delegate { OnQualityChange(); });
+    }
+    
+    public void OnQualityChange()
+    {
+        QualitySettings.SetQualityLevel(qualityDropdown.value, true);
+    }
+    
+    
+    
+    
+}
