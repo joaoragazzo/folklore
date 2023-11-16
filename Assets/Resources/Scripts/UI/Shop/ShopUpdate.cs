bb7b99e6378e3ef3eae6f4b798b238607bbec3bf
João Ragazzo <joao_ragazzo@hotmail.com>
2023-11-16 20:14:59 -0300

    adding icon to the game

--- Assets/Resources/Scripts/UI/Shop/ShopUpdate.cs
+++ Assets/Resources/Scripts/UI/Shop/ShopUpdate.cs
@@ -15,7 +15,8 @@ public class ShopUpdate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(KeyCode.P))
+        
+        if (Input.GetKeyDown(KeyCode.B) && WorldStatsController.Stats.IsDay)
         {
             showShop = !showShop;
             
